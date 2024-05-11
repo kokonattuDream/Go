b21d34c98beaf39e16ef1e4c4b48637fc9f2749c
kokonattuDream <kerjohn.chen@gmail.com>
2024-05-11 18:09:19 -0400

    add registration apis

--- restApi/routes/routes.go
+++ restApi/routes/routes.go
@@ -11,10 +11,11 @@ func RegisterRoutes(server *gin.Engine) {
 
 	authenticated := server.Group("/")
 	authenticated.Use(middlewares.Authenticate)
-	authenticated.POST("/events", middlewares.Authenticate, createEvent)
 	authenticated.POST("/events", createEvent)
 	authenticated.PUT("/events/:id", updateEvent)
 	authenticated.DELETE("/events/:id", deleteEvent)
+	authenticated.POST("/events/:id/register", registerForEvent)
+	authenticated.DELETE("/events/:id/register", cancelRegistration)
 
 	server.POST("/signup", signup)
 	server.POST("/login", login)
